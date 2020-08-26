68da3aac8b94e4fe66f4fcb1f4a953af322494d2
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-08-26 11:19:51 +0900

    Upload the Code
    Upload the Code

---
+++ bot.py
@@ -0,0 +1,21 @@
+from pyautogui import *
+import pyautogui as pq
+import time
+import keyboard
+
+def click(x,y):
+    pq.moveTo(x,y)
+    pq.mouseDown()
+    time.sleep(0.01) #This pauses the script for 0.01 seconds
+    pq.mouseUp()
+
+while keyboard.is_pressed('q') == False:
+    
+    if pq.pixel(480, 552)[0] == 0:
+        click(480, 552)
+    if pq.pixel(590, 561)[0] == 0:
+        click(590, 561)
+    if pq.pixel(700, 571)[0] == 0:
+        click(700, 571)
+    if pq.pixel(800, 569)[0] == 0:
+        click(800, 569)
